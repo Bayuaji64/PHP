be6bfa7758feb7e137cbc5b623776991cc71f2c7
Bayuaji64 <bayuajipradipta64@gmail.com>
2024-02-21 09:28:53 +0700

    create scheduling task/queue feature

--- Laravel/ourmainapp/app/Http/Controllers/PostController.php
+++ Laravel/ourmainapp/app/Http/Controllers/PostController.php
@@ -3,9 +3,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\SendNewPostEmail;
 use App\Models\Post;
+// use App\Mail\NewPostEmail;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class PostController extends Controller
 {
@@ -35,6 +38,10 @@ class PostController extends Controller
 
 
         $newPost = Post::create($incomingFields);
+
+        dispatch(new SendNewPostEmail(['sendTo' => auth()->user()->email, 'name' => auth()->user()->username, 'title' => $newPost->title]));
+
+        // Mail::to(auth()->user()->email)->send(new NewPostEmail(['name' => auth()->user()->username, 'title' => $newPost->title]));
 
         return redirect("/post/{$newPost->id}")->with('success', 'New post successfully created');
     }
---
+++ Laravel/ourmainapp/resources/views/recapemail.blade.php
@@ -0,0 +1,10 @@
+<div style="font-family: sans-serif">
+
+
+    <p>Here is a quick recap of the applicatio.</p>
+    <p>Post Count: <strong>{{$postCount}}</strong></p>
+    <p>User Count: <strong>{{$userCount}}</strong></p>
+  
+
+
+</div>
